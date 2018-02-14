6a4bdef85ffa3fd8af095be4889ec35fd85d568c
Tang Qiming <venux021@outlook.com>
2018-02-14 18:14:35 +0800

    https support

--- xdeploy.sh
+++ xdeploy.sh
@@ -4,19 +4,20 @@ usage()
 {
     cat << EOF
 usage: 
-    $(basename $0) -h <host> [-p <port>] -d <server-id>
+    $(basename $0) -h <host> [-p <port>] -d <server-id> [-s]
 
 options:
     -h <host>        tomcat host ip or host name
     -p <port>        tomcat port, [Default: 8080]
     -d <server-id>   tomcat server id in ~/.m2/settings.xml
+    -s               HTTPS
 EOF
     exit 1
 }
 
-port=8080
+https=no
 
-while getopts ':h:p:d:' OPTION; do
+while getopts ':h:p:d:s' OPTION; do
     case $OPTION in
         h)
             host=$OPTARG
@@ -27,6 +28,9 @@ while getopts ':h:p:d:' OPTION; do
         d)
             server_id=$OPTARG
             ;;
+        s)
+            https=yes
+            ;;
         \?)
             usage
             ;;
@@ -35,8 +39,23 @@ done
 
 [ $# -eq 0 ] && usage
 
+if [ -z "$port" ]; then
+    if [ $https = 'yes' ]; then
+        port=443
+    else
+        port=8080
+    fi
+fi
+
+scheme=http
+[ $https = 'yes' ] && scheme=https
+
+domain=$host:$port
+[ $scheme = 'http' -a $port -eq 80 ] && domain=$host
+[ $scheme = 'https' -a $port -eq 443 ] && domain=$host
+
 # tomcat 7+
-upload_url="http://$host:$port/manager/text"
+upload_url="$scheme://$domain/manager/text"
 
 mvn tomcat7:undeploy tomcat7:deploy-only -Dmaven.test.skip=true -Dtomcat.url="$upload_url" -Dtomcat.server="$server_id"
 
